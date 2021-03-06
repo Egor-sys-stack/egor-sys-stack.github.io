a42e37a09f991077e8c66de77f6ddf554dd3efad
Egor-sys-stack <80152932+Egor-sys-stack@users.noreply.github.com>
2021-03-06 15:13:57 +0500

    Add files via upload

---
+++ precache-manifest.db1ea43630aa7b183033da4dd31e5448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941258b292f8c1d431f44e78985145a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68cc22ff824ee126ae80",
+    "url": "/static/css/2.b84722ed.chunk.css"
+  },
+  {
+    "revision": "68cc22ff824ee126ae80",
+    "url": "/static/js/2.317d03eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.317d03eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2fe494daa3b2e82e437",
+    "url": "/static/js/main.793bc0ab.chunk.js"
+  },
+  {
+    "revision": "b6656e9ee5f05057b532",
+    "url": "/static/js/runtime-main.0132ba8f.js"
+  }
+]);
